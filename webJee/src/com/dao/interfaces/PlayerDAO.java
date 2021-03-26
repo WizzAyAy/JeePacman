9036e9aade3c8d4eb31c35ade87d3f88dd01983f
etudiant <etudiant@d7P2>
2021-03-26 14:58:22 +0100

    hmm

--- webJee/src/com/dao/interfaces/PlayerDAO.java
+++ webJee/src/com/dao/interfaces/PlayerDAO.java
@@ -2,7 +2,7 @@ package com.dao.interfaces;
 import com.bean.User;
 import com.dao.exceptions.DAOException;
 
-public interface PlayerDAO { // Ask if void or type 
+public interface PlayerDAO {
 	void create(User player) throws DAOException;
 	User read(String pseudo) throws DAOException;
 	void updateToken(User player) throws DAOException;
