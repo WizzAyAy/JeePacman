1cc95bd5b4959fe79b0818c687739885b538f62e
etudiant <etudiant@d7P2>
2021-04-02 03:04:37 +0200

    get user cosmetics + cleared some files

--- webJee/src/com/endpoints/UserEndpoint.java
+++ webJee/src/com/endpoints/UserEndpoint.java
@@ -1,6 +1,7 @@
 package com.endpoints;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,10 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.bean.Cosmetic;
 import com.bean.User;
+import com.dao.CosmeticsDAOImpl;
 import com.dao.DAOFactory;
 import com.dao.PlayerDAOImpl;
+import com.dao.interfaces.CosmeticsDAO;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
 import com.mysql.cj.Session;
 
 @WebServlet( name="User", urlPatterns = "/user" )
@@ -29,7 +36,7 @@ public class UserEndpoint extends HttpServlet  {
 	public void doGet( HttpServletRequest request, HttpServletResponse response )	throws ServletException, IOException {
 		
 		String token = Utilities.getCookieValue(request, "token");
-		if(token != null) {		
+		if(token != null && request.getParameter("info")==null) {		
 			User player = playerDao.read(token);
 			String json = new Gson().toJson(player);
 		    response.setContentType("application/json");
@@ -40,13 +47,34 @@ public class UserEndpoint extends HttpServlet  {
         	request.setAttribute("username", player.getUsername());
         	
 			request.setAttribute( "user", json );
+			this.getServletContext().getRequestDispatcher( "/WEB-INF/pages/myInfo.jsp" ).forward( request, response );
+		}
+		else if(token != null && request.getParameter("info").equals("cosmetics"))
+		{
+			CosmeticsDAOImpl cosDao = ((DAOFactory) getServletContext().getAttribute( CONF_DAO_FACTORY )).getCosmeticsDao();
+			
+			ArrayList<Cosmetic> cos = cosDao.readPlayerCosmetics(token);
+
+			Gson gson = new Gson();
+			JsonElement elmt = gson.toJsonTree(cos, new TypeToken<ArrayList<Cosmetic>>() {}.getType());
+			
+			if(!elmt.isJsonArray())
+			{
+				response.setStatus(404); // Didn't know which code to put there, it's 3 rn, TODO
+			}
+			else
+			{
+				JsonArray cosArray = elmt.getAsJsonArray();
+				request.setAttribute("cosmetics", cosArray);
+			}
+			
+			this.getServletContext().getRequestDispatcher( "/WEB-INF/pages/cosmetics.jsp" ).forward( request, response );
 		}
 		else
 		{
 			response.setStatus(401); // Unauthorized
+			this.getServletContext().getRequestDispatcher( "/WEB-INF/pages/myInfo.jsp" ).forward( request, response );
 		}
-		
-		this.getServletContext().getRequestDispatcher( "/WEB-INF/pages/myInfo.jsp" ).forward( request, response );
 	}
 	
 	public void doPut(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException 
